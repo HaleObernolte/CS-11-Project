b0568826f2fe98d52d5b93624448c889cfb37808
Hale <hobernol@caltech.edu>
2020-10-12 01:35:12 -0700

    EasySwap Pass #2
    Added multiple configurations, ability to save talent configurations, saving talent configurations in persistent data, and support for different talent configurations for different specs.

--- EasySwap/EasySwap.lua
+++ EasySwap/EasySwap.lua
@@ -1,15 +1,14 @@
 -- Data ----------------------------------------------------------------
 
 EASY_SWAP = "Easy Swap";
+SAVE_CURRENT_CONFIG = "Save Talents";
+TALENT_CONFIGURATION = "Talent Configuration ";
 
-local testSpec = {
-	3,
-	2,
-	2,
-	1,
-	1,
-	1,
-	1,
+SPEC_CONFIGURATIONS = {
+	{},
+	{},
+	{},
+	{},
 };
 
 -- End Data ------------------------------------------------------------
@@ -64,6 +63,25 @@ local function OnLoadTalentUI()
 	EasySwapFrame:SetPoint("TOPLEFT", PlayerTalentFrame, "TOPRIGHT", ofsX, ofsY);
 end
 
+local function SaveCurrentConfig(spec, config)
+	local newConfig = {};
+	local setTalents = false;
+	for tier = 1, MAX_TALENT_TIERS do
+		for col = 1, 3 do
+			local talentButton = _G["PlayerTalentFrameTalentsTalentRow"..tier.."Talent"..col];
+			local selected = talentButton.knownSelection:IsShown();
+			if ( selected ) then
+				newConfig[tier] = col;
+				setTalents = true;
+				break;
+			end
+		end
+	end
+	if ( setTalents ) then
+		SPEC_CONFIGURATIONS[spec][config] = newConfig;
+	end
+end
+
 local function GetTalentButton(row, col)
 	return _G["PlayerTalentFrameTalentsTalentRow"..row.."Talent"..col];
 end
@@ -72,6 +90,7 @@ end
 local EasySwapFrameEvents = {
 	"ADDON_LOADED",
 	"PLAYER_ENTERING_WORLD",
+	"PLAYER_SPECIALIZATION_CHANGED",
 };
 
 EasySwapFrameMixin = {};
@@ -92,6 +111,15 @@ function EasySwapFrameMixin:OnEvent(event, ...)
 		if ( not IsAddOnLoaded("Blizzard_TalentUI") ) then
 			LoadTalentsTab();
 		end
+		self:Update();
+	elseif ( event == "PLAYER_SPECIALIZATION_CHANGED" ) then
+		self:Update();
+	end
+end
+
+function EasySwapFrameMixin:Update()
+	for _, spec in ipairs(self.EasySwapSpecs) do
+		spec:Update();
 	end
 end
 
@@ -107,12 +135,45 @@ function EasySwapButtonMixin:OnClick(button, up)
 end
 
 
-EasySwapSpecButtonMixin = {};
+EasySwapActivateSpecButtonMixin = {};
 
-function EasySwapSpecButtonMixin:OnClick(button, down)
-	for row, col in ipairs(testSpec) do
-		local talentButton = GetTalentButton(row, col);
-		PlayerTalentFrameTalent_OnClick(talentButton, "LeftButton");
+function EasySwapActivateSpecButtonMixin:OnClick(button, down)
+	local currSpec = GetSpecialization();
+	local configNumber = self:GetParent().configNumber;
+	local configuration = SPEC_CONFIGURATIONS[currSpec][configNumber];
+	if ( configuration ) then
+		for row, col in ipairs(configuration) do
+			local talentButton = GetTalentButton(row, col);
+			PlayerTalentFrameTalent_OnClick(talentButton, "LeftButton");
+		end
+	end
+end
+
+
+EasySwapSaveSpecButtonMixin = {};
+
+function EasySwapSaveSpecButtonMixin:OnClick(button, down)
+	local currSpec = GetSpecialization();
+	local configNumber = self:GetParent().configNumber;
+	SaveCurrentConfig(currSpec, configNumber);
+	self:GetParent():Update();
+end
+
+
+EasySwapSpecMixin = {};
+
+function EasySwapSpecMixin:OnLoad()
+	self.SpecName:SetText(TALENT_CONFIGURATION..self.configNumber);
+end
+
+function EasySwapSpecMixin:Update()
+	local currSpec = GetSpecialization();
+	local configNumber = self.configNumber;
+	local configuration = SPEC_CONFIGURATIONS[currSpec][configNumber];
+	if ( configuration ) then
+		self.ActivateButton:Enable();
+	else
+		self.ActivateButton:Disable();
 	end
 end
 
