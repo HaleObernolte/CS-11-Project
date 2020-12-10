8a0dd875d9c7c0a4be7e56996bb0ec7d2a2e5cca
Hale <hobernol@caltech.edu>
2020-12-10 10:58:42 -0800

    Item Tracker Pass #2
    Added item removal, direct item add from backs, and adding manual gold prices

--- ItemTracker/ItemTracker.lua
+++ ItemTracker/ItemTracker.lua
@@ -3,7 +3,7 @@
 ITEM_TRACKER = "Item Tracker";
 IT_START_SESSION = "Start Session";
 IT_STOP_SESSION = "Stop Session";
-IT_UNKNOWN_VALUE = "--|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t --|TInterface\\MoneyFrame\\UI-SilverIcon:0:0:2:0|t --|TInterface\\MoneyFrame\\UI-CopperIcon:0:0:2:0|t"
+IT_UNKNOWN_VALUE = "--|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t"
 IT_ADD_ITEM_INSTRUCTIONS = "|cFF808080Enter item name or item ID here...|r";
 
 IT_TRACKED_ITEMS = {};
@@ -24,6 +24,23 @@ IT_SETTINGS = {
 
 -- Hooks ---------------------------------------------------------------
 
+hooksecurefunc("ContainerFrameItemButton_OnModifiedClick", function(self, button)
+	if ( button == "RightButton" and IsControlKeyDown() and not IsShiftKeyDown() and not IsAltKeyDown() ) then
+		local bag = self:GetParent():GetID();
+		local slot = self:GetID();
+		local _, _, _, _, _, _, _, _, _, itemID = GetContainerItemInfo(bag, slot);
+		local itemName, _, itemRarity, _, _, _, _, _, _, itemTexture = GetItemInfo(itemID);
+		if ( itemName ) then
+			local newItem = {
+				Texture = itemTexture,
+				Name = itemName,
+				Quality = itemRarity,
+				Quantity = GetItemCount(itemName, IT_SETTINGS.IncludeBankInCount),
+			};
+			ItemTrackerFrame:AddItem(newItem);
+		end
+	end
+end);
 
 -- End Hooks -----------------------------------------------------------
 
@@ -80,6 +97,19 @@ function ItemTrackerFrameMixin:AddItem(item)
 	end
 end
 
+function ItemTrackerFrameMixin:RemoveItem(itemName)
+	if ( IT_ITEM_IS_TRACKED_MAP[itemName] ) then
+		for idx, item in ipairs(IT_TRACKED_ITEMS) do
+			if ( item.Name == itemName ) then
+				tremove(IT_TRACKED_ITEMS, idx);
+				IT_ITEM_IS_TRACKED_MAP[itemName] = nil;
+				self.ScrollList:RefreshListDisplay();
+				return;
+			end
+		end
+	end
+end
+
 function ItemTrackerFrameMixin:OnDragStart()
 	self:StartMoving();
 end
@@ -106,6 +136,10 @@ end
 
 ItemTrackerItemMixin = {};
 
+function ItemTrackerItemMixin:OnLoad()
+	self:RegisterForClicks("AnyUp");
+end
+
 function ItemTrackerItemMixin:InitElement(itemList)
 	self.itemList = itemList;
 end
@@ -124,17 +158,33 @@ function ItemTrackerItemMixin:UpdateDisplay()
 	self.Icon:SetTexture(option.Texture or "Interface\\Icons\\INV_Misc_QuestionMark");
 	self.Name:SetTextColor(GetItemQualityColor(option.Quality or 1));
 	self.Name:SetText(option.Name);
+	self.ItemName = option.Name;
 	self.Quantity:SetText(option.Quantity);
 	if ( option.ValuePer) then
-		local valuePerText = GetCoinTextureString(option.ValuePer);
-		valuePerText = strsub(valuePerText, 1, strfind(valuePerText, "|t") + 1);
-		local valueTotalText = GetCoinTextureString(option.ValuePer * option.Quantity);
-		valueTotalText = strsub(valueTotalText, 1, strfind(valueTotalText, "|t") + 1);
+		local valuePerText, valueTotalText;
+		local goldVal = option.ValuePer / 10000;
+		if ( goldVal > 10000 ) then
+			valuePerText = string.format("%.1f", goldVal / 1000).."k|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t";
+		else
+			valuePerText = string.format("%.0f", goldVal).."|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t";
+		end
+		local totalGold = goldVal * option.Quantity;
+		if ( totalGold > 10000 ) then
+			valueTotalText = string.format("%.1f", totalGold / 1000).."k|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t";
+		else
+			valueTotalText = string.format("%.0f", totalGold).."|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t";
+		end
 		self.ValuePer:SetText(valuePerText);
 		self.ValueTotal:SetText(valueTotalText);
 	else
 		self.ValuePer:SetText(IT_UNKNOWN_VALUE);
 		self.ValueTotal:SetText(IT_UNKNOWN_VALUE);
+	end
+end
+
+function ItemTrackerItemMixin:OnClick(button, down)
+	if ( button == "RightButton" ) then
+		self:GetItemList():RemoveItem(self.ItemName);
 	end
 end
 
@@ -189,6 +239,13 @@ end
 
 function ItemTrackerAddItemBoxMixin:OnEnterPressed()
 	local input = self:GetText();
+	local colonIdx = strfind(input, ":");
+	local price;
+	if ( colonIdx ) then
+		price = strsub(input, colonIdx + 1);
+		price = tonumber(price) * 10000;
+		input = strsub(input, 1, colonIdx - 1);
+	end
 	local itemName, _, itemRarity, _, _, _, _, _, _, itemTexture = GetItemInfo(input);
 	if ( itemName ) then
 		local newItem = {
@@ -196,7 +253,7 @@ function ItemTrackerAddItemBoxMixin:OnEnterPressed()
 			Name = itemName,
 			Quality = itemRarity,
 			Quantity = GetItemQuantity(itemName),
-			ValuePer = 1251111;
+			ValuePer = price,
 		};
 		self:GetParent():AddItem(newItem);
 	end
